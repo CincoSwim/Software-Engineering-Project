0258863fb028e48fde53e2c941e9a2adb36d4f6d
CincoSwim <cgpucko@gmail.com>
2021-04-22 22:28:02 -0400

    Added logic for UserFlightSearch - should refine search with some more testing. Made changes to Transaction object, some ComboBox logic

--- UserFlightSearchWindow.xaml.cs
+++ UserFlightSearchWindow.xaml.cs
@@ -39,5 +39,84 @@ namespace Software_Engineering_Project
         {
             m_parent.Show();
         }
+
+        private void BookFlightButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(FoundFlightsGrid.SelectedItem == null)
+            {
+                MessageBox.Show("Please select an item.", "Alert", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {   FlightManifestObj selected = (FlightManifestObj)FoundFlightsGrid.SelectedItem;
+                //Confirm, then book the flight
+                if (MessageBox.Show("Are you sure you want to book the following flight? \n Flight ID: " + selected.flightID +
+                     "\n Departs From: " + selected.originCode + "\n Departure Time: " + selected.departTime.ToLongDateString(),
+                     "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No)
+                {
+                    //do no stuff
+                    Console.WriteLine("Selected \"no\", no flight booked");
+                }
+                else
+                {
+                    //do yes stuff
+                    App.LoggedInUser.upcomingFlights.Add(selected.flightID.ToString());
+                    App.UserAccountDict[App.LoggedInUser.uniqueID].upcomingFlights.Add(selected.flightID.ToString());
+                    if(App.LoggedInUser.balance > selected.ticketPrice)
+                    {
+                        if((MessageBox.Show("Would you like to use your Points? \n Flight Price: " + selected.ticketPrice +
+                                            "\n Account Balance: " + App.LoggedInUser.balance,"Question", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No))
+                        {
+                            //doesn't use points
+                            TransactionObj newTransact = new TransactionObj();
+                            newTransact.FlightUID = selected.flightID.ToString();
+                            newTransact.UserUID = App.LoggedInUser.uniqueID;
+                            newTransact.transactionAmt = selected.ticketPrice;
+                        }
+                        else
+                        {
+                            //does use points
+                            TransactionObj newTransact = new TransactionObj();
+                            newTransact.FlightUID = selected.flightID.ToString();
+                            newTransact.UserUID = App.LoggedInUser.uniqueID;
+                            newTransact.transactionAmt = 0;
+
+                            App.LoggedInUser.balance = App.LoggedInUser.balance - selected.ticketPrice;
+                            App.UserAccountDict[App.LoggedInUser.uniqueID].balance = App.UserAccountDict[App.LoggedInUser.uniqueID].balance - selected.ticketPrice;
+
+                        }
+                    }
+                    else
+                    {
+                        //doesn't use points -- because there isn't enough!
+                        TransactionObj newTransact = new TransactionObj();
+                        newTransact.FlightUID = selected.flightID.ToString();
+                        newTransact.UserUID = App.LoggedInUser.uniqueID;
+                        newTransact.transactionAmt = selected.ticketPrice;
+                    }
+                }
+            }
+        }
+
+        private void SearchRefreshBtn_Click(object sender, RoutedEventArgs e)
+        {
+            List<FlightManifestObj> searchList = new List<FlightManifestObj>();
+
+            foreach (var flight in App.FlightPlanDict)
+            {
+                if(flight.Value.originCode == DepartureSelectionBox.SelectedValue.ToString())
+                {
+                    if(ArrivalSelectionBox.SelectedValue.ToString() == flight.Value.layoverCodeA || ArrivalSelectionBox.SelectedValue.ToString() == flight.Value.layoverCodeB || ArrivalSelectionBox.SelectedValue.ToString() == flight.Value.destinationCode)
+                    {
+                        if(ArriveTimePick.Value == null || DepartTimePick.Value == null)
+                        {
+                            searchList.Add(flight.Value);
+                        }else if(DepartTimePick.Value > flight.Value.departTime){
+                            searchList.Add(flight.Value);
+                        }
+                    }
+                }
+            }
+            FoundFlightsGrid.ItemsSource = searchList;
+        }
     }
 }
