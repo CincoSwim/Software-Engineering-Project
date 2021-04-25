37df4b2f6c0ef48e2861f9564dc1af7a199117c5
CincoSwim <cgpucko@gmail.com>
2021-04-25 16:23:44 -0400

    chagne to lambda expression

--- UserLandingWindow.xaml.cs
+++ UserLandingWindow.xaml.cs
@@ -41,7 +41,7 @@ namespace Software_Engineering_Project
             var periodSpan = TimeSpan.FromMinutes(5);
 
             System.Threading.Timer timer2 = null;
-            timer2 = new System.Threading.Timer(_ => UpcomingFlightsGrid.Items.Refresh());
+            timer2 = new System.Threading.Timer(e => UpcomingFlightsGrid.Items.Refresh());
             timer2.Change(startSpan, periodSpan);
 
         }
